52bacde135b87335a3b5c2b80d39a04c57c4ee6b
drherobrine <tim.melentyev@gmail.com>
2016-11-22 18:44:50 +0100

    Initial Commit

--- game.py
+++ game.py
@@ -1,114 +1,157 @@
-import time
-import random
+import random #Note: the table looks like this: 0   1   2
+import time   #                                 3   4   5
+import sys    #                                 6   7   8
+
+number = 27
+playerNumber = 25
 
 
+moveMemory = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+blackList = ["asd", 23]
 
-def tuthack():
-    fail = 0
-    win = 0
-    tutpass = "back"
-    print('The password hint is "chair"')
-    for x in range(0,4):
-        hackTry = input("Type password here\n")
-        if hackTry == tutpass:
-            print("Access Granted")
-            win = 1
-            break
+move = False
+
+class Slot():
+
+    _isOccupied = False
+    _isCross = False
+    _isCircle = False
+
+    def __init__(self, isOccupied, isCross, isCircle):
+        self._isOccupied = isOccupied
+        self._isCross = isCross
+        self._isCircle = isCircle
+
+    def isSlotOccupied(self):
+        if self._isOccupied == True:
+            return self._isOccupied
+
+    def isSlotCross(self):
+        if self._isCross == True:
+            self._isOccupied = True
+            return self._isCross
+
+    def isSlotCircle(self):
+        if self._isCircle == True:
+            self._isOccupied = True
+            return self._isCircle
+
+
+instances = [Slot(False, False, False), Slot(False, False, False), Slot(False, False, False), Slot(False, False, False), Slot(False, False, False), "x3y2", "x1y3", "x2y3", "x3y3"]
+for i in range(0,9):
+    instances[i] = Slot(False, False, False)
+
+players = ["Player"]
+starter = random.choice(players)
+
+print("Principles of the game: . = unoccupied, X = occupied by You, O = occupied by CPU.")
+print("%s begins!"%starter)
+
+def winCheck():
+    #Check Horizontal wins
+    if instances[0]._isCross and instances[1]._isCross and instances[2]._isCross or instances[3]._isCross and instances[4]._isCross and instances[5]._isCross or instances[6]._isCross and instances[7]._isCross and instances[8]._isCross:
+        print("You won!")
+        return "win"
+    #Check for Vertical wins
+    elif instances[0]._isCross and instances[3]._isCross and instances[6]._isCross or instances[1]._isCross and instances[4]._isCross and instances[7]._isCross or instances[2]._isCross and instances[5]._isCross and instances[8]._isCross:
+        print("You won!")
+        return "win"
+    #Check for diagonal wins
+    elif instances[0]._isCross and instances[4]._isCross and instances[8]._isCross or instances[2]._isCross and instances[4]._isCross and instances[6]._isCross:
+        print("You won!")
+        return "win"
+
+
+    # Check Horizontal losses
+    if instances[0]._isCross and instances[1]._isCircle and instances[2]._isCircle or instances[3]._isCircle and instances[4]._isCircle and instances[5]._isCircle or instances[6]._isCircle and instances[7]._isCircle and instances[8]._isCircle:
+        print("You lost. :(")
+        return "win"
+    # Check for Vertical losses
+    elif instances[0]._isCross and instances[3]._isCircle and instances[6]._isCircle or instances[1]._isCircle and instances[4]._isCircle and instances[7]._isCircle or instances[2]._isCircle and instances[5]._isCircle and instances[8]._isCircle:
+        print("You lost. :(!")
+        return "win"
+    # Check for diagonal losses
+    elif instances[0]._isCircle and instances[4]._isCircle and instances[8]._isCircle or instances[2]._isCircle and instances[4]._isCircle and instances[6]._isCircle:
+        print("You lost. :(")
+        return "win"
+
+
+def cpuMove():
+    for i in range(0,9):
+        if instances[i]._isOccupied == False:
+            sys.stdout.write(".   ")
+        elif instances[i]._isCross == True:
+            sys.stdout.write("X   ")
+        elif instances[i]._isCircle == True:
+            sys.stdout.write("O   ")
         else:
-            print("Access Denied")
-            fail += 1
-    if fail == 4 and win == 0:
-        print("Unfortunately, you failed.")
+            sys.stdout.write("????")
+        if i == 2 or i == 5 or i == 8:
+            print("\n")
+    number = 23
+    while number in blackList:
+        number = random.choice(moveMemory)
+    blackList.append(number)
+    instances[number]._isCircle = True
+    instances[number]._isOccupied = True
+
+
+def plrMove():
+    print("\nYour turn!")
+    for i in range(0,9):
+        if instances[i]._isOccupied == False:
+            sys.stdout.write(".   ")
+        elif instances[i]._isCross == True:
+            sys.stdout.write("X   ")
+        elif instances[i]._isCircle == True:
+            sys.stdout.write("O   ")
+        else:
+            sys.stdout.write("Err ")
+        if i == 2 or i == 5 or i == 8:
+            print("\n")
+    while playerNumber in blackList:
+        playerNumber = 1
+        playerNumber = int(input("Type your move here(" + str(moveMemory) + "):"))
+    blackList.append(playerNumber)
+    print(playerNumber)
+    instances[playerNumber].isCross = True
+    instances[playerNumber].isOccupied = True
+
+#instances[3]._isCross = True
+#instances[3]._isOccupied = True
+
+
+while True:
+    for i in range(0,9):
+        if instances[i]._isOccupied == False:
+            sys.stdout.write(".   ")
+        elif instances[i]._isCross == True:
+            sys.stdout.write("X   ")
+        elif instances[i]._isCircle == True:
+            sys.stdout.write("O   ")
+        else:
+            sys.stdout.write("Err ")
+        if i == 2 or i == 5 or i == 8:
+            print("\n")
+    if starter == "Player":
+        playerNumber = int(input("Type your move here("+str(moveMemory)+"):"))
+        instances[playerNumber]._isCross = True
+        instances[playerNumber]._isOccupied = True
+        move = False
+
+    if move == False:
+        print("CPU's turn!")
+        cpuMove()
+        move = True
     else:
-        print("Congratulations, You Have Successfully Finished the Tutorial!")
-        win = 0
-        fail = 0
+        print("Your Turn!")
+        plrMove()
+        move = False
+    didWin = winCheck()
 
-def hack(answer, hint):
-    fail = 0
-    password = answer
-    print('The password hint is ', '"', hint, '"')
-    for x in range(0,4):
-        hackTry = input("Type password here\n")
-        if hackTry == password:
-            print("Access Granted")
-            print("Success!")
-            return True
-        else:
-            print("Access Denied")
-            fail += 1
-
-    print("Unfortunately, you failed.")
-    return False
-
-def storymode():
-    print("FRIEND: Nick, I've heard Nuke-clear Labs will destroy Mars in an Hour!! ")
-    time.sleep(1.5)
-    print("WATCH: beepbeep")
-    time.sleep(0.5)
-    print("FRIEND: No! The Nuke is already in Mars Orbit! I know you're a good Hacker!\n Could you hack the probe to get it away from Mars?")
-    time.sleep(5)
-    print("YOU: I sure can, Jack!")
-    time.sleep(1)
-    print("KEYBOARD: clickclickclickclickclickclickclick")
-    time.sleep(0.5)
-    print("PROBE: Breach in level 1 security.")
-    win = hack("kitchen", "oven")
-    if win == False:
-        time.sleep(2)
-        print("Mars Exploded. Game Over.")
+    if didWin == "win":
+        break
     else:
-        win = 0
-        time.sleep(1)
-        print("YOU, JACK: Yes!")
-        time.sleep(1)
-        print("PROBE: Ready to deorbit weapon.")
-        time.sleep(1)
-        print("YOU, JACK: Ungh!")
-        time.sleep(1)
-        print("PROBE: To destroy bomb, you must authenticate")
-        time.sleep(0.9)
-        win = hack("high", "balcony")
-        if win == False:
-            time.sleep(2)
-            print("Mars Exploded. Game Over.")
-            fail = 0
-        else:
-            win = 0
-            fail = 0
-            time.sleep(1)
-            print("PROBE: Disassembled weapon into 3lem3n7ary particg3uc")
-            time.sleep(0.5)
-            print("YOU, JACK: Hurray!")
-            time.sleep(1)
-            print("And that concludes the amazing story of Nick and Jack.")
+        pass
 
 
-
-def tutorial():
-    print("Welcome to the Tutorial!")
-    time.sleep(1)
-    print("In WTH, you'll have to hack (duh) into Nuke-clear NRL and\n find out what they're up to.")
-    time.sleep(3)
-    print("To do that, You're going to have to do some logical thinking.")
-    time.sleep(2)
-    print('''If you got a hint saying "bathtub", you'll need to think\n of something that goes together with bathtubs.''')
-    time.sleep(3)
-    print('You might think, "faucet" or "water". Try out all the results. One of them will be the password.')
-    time.sleep(3)
-    print("Let's try an example.")
-    time.sleep(2)
-    tuthack()
-
-print('Hello, and welcome to What the Hack.')
-startTut = input("Do you wish to see the Tutorial?\n(y,n) ").lower()
-if startTut == "y":
-    tutorial()
-    time.sleep(1)
-    print("\nAnd now, Time for the story mode!\n")
-    time.sleep(1)
-    storymode()
-elif startTut == "n":
-    storymode()
-else:
-    print("Error: unrecognized value")
